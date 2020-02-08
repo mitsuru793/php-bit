1ab86d2a9aeaeb2ec92a70a608118c2c37676c5b
mitsuru793 <mitsuru793@gmail.com>
2020-02-09 00:27:27 +0900

    modify namespace and package name

--- examples/example.php
+++ examples/example.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-use Php\UnlimitedBit;
+use Mitsuru793\Bit\UnlimitedBit;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
--- src/UnlimitedBit.php
+++ src/UnlimitedBit.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Php;
+namespace Mitsuru793\Bit;
 
 class UnlimitedBit implements Bit
 {
